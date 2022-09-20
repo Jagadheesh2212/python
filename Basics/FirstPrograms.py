0c9c8bc23cd890216b3c8094f97470b8f72aa378
Jagadheesh2212 <jagadheesh2212@gmail.com>
2022-09-20 19:30:31 +0530

    Update FirstPrograms.py

--- Basics/FirstPrograms.py
+++ Basics/FirstPrograms.py
@@ -1,12 +1,17 @@
-print("hello world")
-a=1
-b=5
-c=a+b
-print(c)
-N=100
-M=85
-K=N%M
-print(K)
+# print("hello world")
+# a=1
+# b=5
+# c=a+b
+# print(c)
+# N=100
+# M=85
+# K=N%M
+# print(K)
 
 
+name=input("Enter Name\n")
+age=input("Enter age\n")
+age2=int(age)+1
+height=input("Enter Height\n")
 
+print("Hi ",name," your age is ", age2 , " and height is ",height)
